9cc9ea58ba3679471606435861d076a56887446e
Dekopossas <andrepossaster@gmail.com>
2022-08-17 17:09:17 -0300

    add news inputs

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -12,20 +12,21 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [institutName, setInstitutName] = useState('');
+  const [institutJson, setInstitutJson] = useState('');
+  const [trabalhabilit, setTrabalhabilit] = useState('');
+  const [institutSeed, setInstitutSeed] = useState('');
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    if (!newTaskTitle) return;
-
     const newTask = {
       id: Math.random(),
-      title: newTaskTitle,
+      // title: newTaskTitle,
       isComplete: false,
     };
 
-    setTasks((oldState) => [...oldState, newTask]);
-    setNewTaskTitle('');
+    // setTasks((oldState) => [...oldState, newTask]);
+    // setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
@@ -55,10 +56,31 @@ export function TaskList() {
 
         <div className="input-group">
           <input
+            style={{ width: '100px' }}
             type="text"
-            placeholder="Adicionar novo todo"
-            onChange={(e) => setNewTaskTitle(e.target.value)}
-            value={newTaskTitle}
+            placeholder="Instituição"
+            onChange={(e) => setInstitutName(e.target.value)}
+            value={institutName}
+          />
+          <input
+            type="text"
+            placeholder="Json da Instituição"
+            onChange={(e) => setInstitutJson(e.target.value)}
+            value={institutJson}
+          />
+            <input
+              style={{ width: '120px' }}
+              type="text"
+              placeholder="Trabalhabilidade"
+              onChange={(e) => setTrabalhabilit(e.target.value)}
+              value={trabalhabilit}
+            />
+          <input
+            style={{ width: '100px' }}
+            type="text"
+            placeholder="Candidaturas"
+            onChange={(e) => setInstitutSeed(e.target.value)}
+            value={institutSeed}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff" />
@@ -80,6 +102,24 @@ export function TaskList() {
             <th>Trabalhabilidade</th>
             <th>Candidaturas</th>
             <th>Actions</th>
+          </tr>
+          {/* {instituteTable.map((institute) => {})} */}
+          <tr>
+            <td>UVA</td>
+            <td>12345125</td>
+            <td>123</td>
+            <td>432</td>
+            <td>151</td>
+            <td>6123</td>
+            <td>43</td>
+            <td>12</td>
+            <td>4321</td>
+            <td>43</td>
+            <td>
+              <button> \/ </button>
+              <button> X </button>
+              <button> /\ </button>
+            </td>
           </tr>
           <tr>
             <td>UVA</td>

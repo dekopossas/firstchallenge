ceb42a913e0384c9014aa22375b28aa96423f5ff
Dekopossas <andrepossaster@gmail.com>
2022-08-29 15:50:30 -0300

    ajusts about name

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -66,7 +66,7 @@ export function TaskList() {
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>Permanências</h2>
 
         <div className="input-group">
           <input
